cf04baa210dfd238eaee973231c597dfe98378b3
Gabriel dos Santos <gabrields1910@gmail.com>
2024-04-29 10:47:07 -0300

    feat - geolocalization service consumer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,22 +1,71 @@
-class GeoLib {
-    public getAddressFromCoordinates(coordinates: [number, number] | { lat: number; lng: number }): Promise<string> {
-        try {
-            const address = 'Rua fulano de tal'
-            return Promise.resolve(address)
-        } catch (error) {
-            return Promise.reject(new Error('Error to get address'));
-        }
-    };
+// class GeoLib {
+//     public getAddressFromCoordinates(coordinates: [number, number] | { lat: number; lng: number }): Promise<string> {
+//         try {
+//             const address = 'Rua fulano de tal'
+//             return Promise.resolve(address)
+//         } catch (error) {
+//             return Promise.reject(new Error('Error to get address'));
+//         }
+//     };
 
-    public getCoordinatesFromAddress(address: string): Promise<[number, number]> {
+//     public getCoordinatesFromAddress(address: string): Promise<[number, number]> {
+//         try {
+//             // const coordinates = { lat: 1, lng: 2 }
+//             const coordinates: [number, number] = [1, 2]
+//             return Promise.resolve(coordinates)
+//         } catch (error) {
+//             return Promise.reject(new Error('Error to get coordinates'));
+//         }
+//     };
+// }
+
+// export default new GeoLib();
+
+
+import axios from 'axios';
+
+class GeoLib {
+    async getAddressFromCoordinates(coordinates: any) {
+        console.log("Teste 1 ===>>", coordinates)
         try {
-            // const coordinates = { lat: 1, lng: 2 }
-            const coordinates: [number, number] = [1, 2]
-            return Promise.resolve(coordinates)
+            let response;
+            if (Array.isArray(coordinates)) {
+                response = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${coordinates[0]}&lon=${coordinates[1]}`, {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                });
+                console.log("Teste 3 ===>>", response?.data)
+            } else {
+                throw new Error('Invalid coordinates format');
+            }
+
+            if (response?.data?.display_name) {
+                return `${response.data.display_name}`;
+            } else {
+                throw new Error('Address not found');
+            }
         } catch (error) {
-            return Promise.reject(new Error('Error to get coordinates'));
+            throw new Error('Error to get address');
         }
-    };
+    }
+
+    async getCoordinatesFromAddress(address: any) {
+        try {
+            const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`);
+
+            if (Array.isArray(response.data) && response.data.length > 0) {
+                const { lat, lon } = response.data[0];
+                return [parseFloat(lat), parseFloat(lon)];
+            } else {
+                throw new Error('Coordinates not found');
+            }
+        } catch (error) {
+            console.error('Error getting coordinates:', error);
+            throw new Error('Error to get coordinates');
+        }
+    }
 }
 
 export default new GeoLib();
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,13 @@
 import lib from '../lib';
 
 
-export const resolveCoordinatesAndAddress = async (address: string, coordinates: [number, number]) => {
+export const resolveCoordinatesAndAddress = async (address: string, coordinates: number[]) => {
     if ((address && coordinates) || (!address && !coordinates)) {
         return { error: 'Please provide either address or coordinates' };
     }
-    let userCoordinates: [number, number] | undefined = coordinates
+    let userCoordinates: number[] | undefined = coordinates
     let userAddress: string | undefined = address
+
 
     if (address) {
         userCoordinates = await lib.getCoordinatesFromAddress(address);
