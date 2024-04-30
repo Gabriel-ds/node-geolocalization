ade9733dadf77d36e81d0178e2e51bc3bf088d5e
Gabriel dos Santos <gabrields1910@gmail.com>
2024-04-29 21:49:37 -0300

    remove files

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-    res.json({ success: true });
-})
-
-export default router;
